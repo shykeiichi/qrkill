86597823af18e69efe0d0df0956ef44758b261e4
Movitz Sunar <movitz.sunar@gmail.com>
2019-08-21 19:35:20 +0200

    login with email

--- login.php
+++ login.php
@@ -19,11 +19,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET')
 	die();
 }
 
+$username = explode('@', $_POST['username'])[0];
+
 $ldap = ldap_connect("ldaps://ad.ssis.nu") or die('Något gick fel. Vänligen kontakta Movitz.');
-$bind = ldap_bind($ldap, $_POST['username'] . "@ad.ssis.nu", $_POST['password']);
+$bind = ldap_bind($ldap, $username . "@ad.ssis.nu", $_POST['password']);
 
 $sql = 'SELECT id, name, is_admin, class FROM qr_users WHERE username = ?';
-$user = DB::prepare($sql)->execute([$_POST['username']])->fetch();
+$user = DB::prepare($sql)->execute([$username])->fetch();
 
 if(!$bind)
 {
@@ -41,7 +43,7 @@ if($user)
 	$sql = "INSERT INTO qr_logins (success, qr_users_id) VALUES (1, ?)"; 
 	DB::prepare($sql)->execute([$user['id']]);
 
-	$_SESSION['qr']['username'] = $_POST['username'];
+	$_SESSION['qr']['username'] = $username;
 	$_SESSION['qr']['id'] = $user['id'];
 	$_SESSION['qr']['is_admin'] = $user['is_admin'];
 	$_SESSION['qr']['name'] = $user['name'];
@@ -51,7 +53,7 @@ if($user)
 	die();
 }
 
-$search = ldap_search($ldap, "OU=Elever,DC=ad,DC=ssis,DC=nu", "(cn=" . $_POST['username'] . ")", array("cn", "givenName", "sn", "memberOf")) or die('ldap_search failed');
+$search = ldap_search($ldap, "OU=Elever,DC=ad,DC=ssis,DC=nu", "(cn=" . $username . ")", array("cn", "givenName", "sn", "memberOf")) or die('ldap_search failed');
 $userInfo = ldap_get_entries($ldap, $search);
 if($userInfo['count'] == 0)
 {
@@ -76,11 +78,11 @@ $sql = 'SELECT (COUNT(*) = 0) FROM qr_users';
 $isAdmin = DB::prepare($sql)->execute()->fetchColumn();
 
 $sql = 'INSERT INTO qr_users (username, name, class, is_admin) VALUES (?, ?, ?, ?)';
-DB::prepare($sql)->execute([$_POST['username'], $name, $class, $isAdmin]);
+DB::prepare($sql)->execute([$username, $name, $class, $isAdmin]);
 
 $userId = DB::lastInsertId();
 $_SESSION['qr']['id'] = $userId;
-$_SESSION['qr']['username'] = $_POST['username'];
+$_SESSION['qr']['username'] = $username;
 $_SESSION['qr']['is_admin'] = $isAdmin;
 $_SESSION['qr']['name'] = $name;
 $_SESSION['qr']['class'] = $class;
